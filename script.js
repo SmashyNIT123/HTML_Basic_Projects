ea48d623a5ba9540b03988e2cf2c3e1e808d27af
SmashyNIT123 <n.k.200pandey@gmail.com>
2026-03-01 12:44:42 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,29 +1,21 @@
-let div = document.createElement('div');
-div.id = 'container';
-const h1 = document.createElement('h1');
-h1.innerText = 'Otp Generator';
-div.appendChild(h1);
+const display = document.getElementById("display");
 
-const button = document.createElement('button');
-button.id='btn';
-button.innerHTML = 'Generate OTP';
-const input = document.createElement('input');
-input.id = 'otp';
-input.type= 'number';
-input.placeholder = 'Enter OTP';
-div.appendChild(input);
-div.appendChild(button);
-document.body.appendChild(div);
-//Interaction of otp
+function appendValue(value) {
+    display.value += value;
+}
 
-const button1 = document.getElementById('btn');
-button1.addEventListener('click',function(){
-    const number = Math.floor(Math.random()*1000000);
-    const inputValue = document.getElementById('otp');
-    const p = document.createElement('p');
-    p.id='value';
-    p.innerHTML = 'Generated OTP is :' + number;
-    div.appendChild(p);
-    
-})
+function clearDisplay() {
+    display.value = "";
+}
 
+function deleteLast() {
+    display.value = display.value.slice(0, -1);
+}
+
+function calculate() {
+    try {
+        display.value = eval(display.value);
+    } catch {
+        display.value = "Error";
+    }
+}
